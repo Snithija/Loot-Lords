d382e2567af150b5991fe234cad11224c6ce62c1
bhavyasri00 <alavalabhavyanagasri390@gmail.com>
2025-10-07 01:36:40 +0530

    Simplify App.jsx and vercel.json for stable deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 import Routes from "./Routes";
-import { CartProvider, useCart } from "./context/CartContext";
+import { CartProvider } from "./context/CartContext";
 import { FavoritesProvider } from "./context/FavoritesContext";
-import Notification from "./components/common/Notification";
-import ErrorBoundary from "./components/common/ErrorBoundary";
-import AppInitializer from "./components/common/AppInitializer";
-
-function AppContent() {
-  const { notification } = useCart();
-
-  return (
-    <ErrorBoundary>
-      <Routes />
-      <Notification message={notification} />
-    </ErrorBoundary>
-  );
-}
 
 function App() {
   return (
-    <ErrorBoundary>
-      <CartProvider>
-        <FavoritesProvider>
-          <AppContent />
-        </FavoritesProvider>
-      </CartProvider>
-    </ErrorBoundary>
+    <CartProvider>
+      <FavoritesProvider>
+        <Routes />
+      </FavoritesProvider>
+    </CartProvider>
   );
 }
 
