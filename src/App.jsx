add60f371e13f5f2371ce7f366daac56e90d7720
bhavyasri00 <alavalabhavyanagasri390@gmail.com>
2025-10-06 20:26:27 +0530

    fix: Add missing FavoritesContext and dependencies for search functionality
    - Added FavoritesContext.jsx with complete favorites management
    - Updated App.jsx to include FavoritesProvider wrapper
    - Added Favorites page component
    - Fixed import dependencies for Header search functionality
    - Search autocomplete should now work properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import Routes from './Routes';
+import Routes from "./Routes";
+import { CartProvider, useCart } from "./context/CartContext";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import Notification from "./components/common/Notification";
+
+function AppContent() {
+  const { notification } = useCart();
+
+  return (
+    <>
+      <Routes />
+      <Notification message={notification} />
+    </>
+  );
+}
 
 function App() {
   return (
-        <Routes />
+    <CartProvider>
+      <FavoritesProvider>
+        <AppContent />
+      </FavoritesProvider>
+    </CartProvider>
   );
 }
 
